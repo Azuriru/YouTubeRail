cbd395cef8c75385ad242e18321014434f82537b
Robyn <32017811+GameModerator@users.noreply.github.com>
2019-02-16 21:45:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -101,7 +101,8 @@
                 width: '100%',
                 height: '100%',
                 position: 'absolute',
-                'z-index': '-1'
+                'z-index': '-1',
+                'overflow-y': 'auto'
             }
         }),
         $('<section>', {
